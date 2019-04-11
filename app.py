ced243b1b5c46e8e3a0d2f9cf53442feba6eddf0
Vlad Comanescu <alu0100968590@ull.edu.es>
2019-04-11 14:45:51 +0100

    plot-charts

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, send_file
 
-from charts import get_main_image
+from charts import get_main_image, get_city_image
 from user_database import data
 
 app = Flask(__name__)
@@ -18,6 +18,20 @@ def main_plot():
     """The view for rendering the scatter chart"""
     img = get_main_image()
     return send_file(img, mimetype='image/png', cache_timeout=0)
+
+@app.route('/city/<int:city_id>')
+def city(city_id):
+    """Views for the city details"""
+    city_record = data.get(city_id)
+    return render_template('city.html', city_name=city_record.city_name, city_id=city_id,
+                           city_climate=city_record.city_climate)
+
+
+@app.route('/city<int:city_id>.png')
+def city_plot(city_id):
+    """Views for rendering city specific charts"""
+    img = get_city_image(city_id)
+    return send_file(img, mimetype='image/png', cache_timeout=0,)
 
 def hello_world():
     return 'Hello World!'
